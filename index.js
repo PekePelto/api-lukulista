97beee027304c267079c5a360c284f263caf0853
PekePelto <90453910+PekePelto@users.noreply.github.com>
2021-10-08 14:23:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,17 +7,16 @@ function arvoNumero(alaraja, ylaraja) {
   return Math.floor(ylaraja * Math.random()) + alaraja
 }
 
-]
+
 app.get('/', function (req, res) {
 let numerolista = [
     arvoNumero(0,5),
     arvoNumero(10,15),
     arvoNumero(100,500),
     arvoNumero(10,20),
-    arvoNumero(0,5),
+    arvoNumero(0,5)
   ]
   res.json(numerolista)
-  
-  })
+})
 
 app.listen(PORT)
