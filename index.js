81acd2ac45682a0036a06f7afb35b47e58f46cbc
PekePelto <90453910+PekePelto@users.noreply.github.com>
2021-10-08 14:15:46 +0300

    Numerolista

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const PORT = process.env.PORT || 5000
+
 const app = express()
 
+function arvoNumero(alaraja, ylaraja) {
+  return Math.floor(ylaraja * Math.random()) + alaraja
+}
+
+let numerolista = [
+  arvoNumero(0,5),
+  arvoNumero(10,15),
+  arvoNumero(100,500),
+  arvoNumero(10,20),
+  arvoNumero(0,5),
+]
 app.get('/', function (req, res) {
-  res.json([1, 5, 8, 2, 5])
+  res.json(numerolista)
 })
 
 app.listen(PORT)
