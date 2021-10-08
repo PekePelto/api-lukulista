674c4e962cadc139253961ae512fc59db1d1a8a8
PekePelto <90453910+PekePelto@users.noreply.github.com>
2021-10-08 14:18:45 +0300

    numerolistan siirto

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ function arvoNumero(alaraja, ylaraja) {
   return Math.floor(ylaraja * Math.random()) + alaraja
 }
 
+]
+app.get('/', function (req, res) {
+  res.json(numerolista)
 let numerolista = [
   arvoNumero(0,5),
   arvoNumero(10,15),
   arvoNumero(100,500),
   arvoNumero(10,20),
   arvoNumero(0,5),
-]
-app.get('/', function (req, res) {
-  res.json(numerolista)
 })
 
 app.listen(PORT)
